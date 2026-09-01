92bbf3d0401b2e8d276c8e177b093cfe061e800d
Gearfit89 <jscram1775@gmail.com>
2026-09-01 18:52:23 -0400

    feat: add username logic

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -11,6 +11,10 @@ import { revalidatePath } from "next/cache";
 
 
 import { SignJWT, jwtVerify } from "jose";
+import { createClient } from "./supabase/server";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { sign } from "crypto";
+import { log } from "console";
 
 
 
@@ -97,4 +101,94 @@ export async function getToken():Promise< ReturnData<string> >{
         }
     }
     
+}
+/**
+ * - A helper function that takes the serverClient and checks to see if a username 
+ * is unique.
+ * @throws usernameCheckErr - The actuall supabase call fails
+ * @throws Username is taken - When another username is found
+ * @param serverClient The supabase server client required for the db call
+ * @param username The username that is being check, to see if it is taken
+ */
+export async function isUsernameUnique(serverClient: SupabaseClient, username: string){
+  
+
+
+  const { data, error: usernameCheckErr } = await serverClient
+  .from("profiles")
+  .select("username")
+  .eq("username", username)
+
+  if(usernameCheckErr){
+    throw new Error(usernameCheckErr.message)
+
+  }
+
+if (data?.length > 0) {
+  throw new Error("Username is taken")
+
+}
+
+ 
+
+
+}
+interface SignUpData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export async function signUp({username, email, password}: SignUpData) {
+
+ try{
+
+  const serverClient = await createClient();
+
+  await isUsernameUnique(serverClient, username);
+
+  const { error : signUpErr, data } = await serverClient.auth.signUp({
+    email,
+    password
+  })
+  const userId = data.user?.id;
+
+    if(signUpErr || !userId ){
+      throw new Error(signUpErr?.message)
+    }
+    
+    const { error: profileErr } = await serverClient
+    .from("profiles")
+    .insert({
+      username,
+      id: userId
+    })
+    
+
+
+    if(profileErr){
+      throw new Error(profileErr.message)
+    }
+
+    const { error: logInErr } = await serverClient.auth.signInWithPassword({
+      email,
+      password
+    })
+    
+
+    if(logInErr){
+      throw new Error(logInErr.message)
+    }
+
+  } catch(e: any){
+
+    console.error(e);
+
+    return {
+      success: false,
+      error: e.message
+    }
+
+  }
+  
 }
